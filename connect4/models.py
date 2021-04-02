b42642cdb5aa5f296e2398cbd845e8e93240114d
devikashanbhag <devika16.ds@gmail.com>
2021-04-02 13:43:23 -0700

    Move images under static images folder

--- connect4/models.py
+++ connect4/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-    image = models.FileField(default='connect4/static/connect4/profile.jpg', upload_to="connect4/static/connect4/images")
+    image = models.FileField(default='connect4/static/connect4/images/profile.jpg', upload_to="connect4/static/connect4/images")
     content_type = models.CharField(max_length=50, default='image/jpeg')
     primary_color = models.CharField(max_length=7)
     secondary_color = models.CharField(max_length=7)
